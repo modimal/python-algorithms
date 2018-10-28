29b481ac35a9674692a74fbdd188b850161aa6cb
Arsen Bekzhanov <liwidne@gmail.com>
2018-10-28 17:35:25 +0300

    ДЗ Урок 1. Задание 8.
    Написал скрипт, определяющий високосный ли год.

---
+++ hw_001/exercise_008.py
@@ -0,0 +1,10 @@
+#! usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    year = int(input("Введите год: "))
+
+    if year % 4 == 0 and (not year % 100 == 0 or (year % 100 == 0 and year % 400 == 0)):
+        print("Год високосный.")
+    else:
+        print("Год невисокосный.")
