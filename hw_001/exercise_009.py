d310b2e44c32f4a93969f8843c1055b3a092b073
Arsen Bekzhanov <liwidne@gmail.com>
2018-10-28 17:40:58 +0300

    ДЗ Урок 1. Задание 9.
    Написал скрипт, ищущий среднее число.

---
+++ hw_001/exercise_009.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    print("\nВведите три разных числа (целое или с плавающей точкой):")
+    a, b, c = float(input("a: ")), float(input("b: ")), float(input("c: "))
+
+    if b > a > c or c > a > b:
+        print("Среднее число: {}".format(int(a) if a.is_integer() else a))
+    elif a > b > c or c > b > a:
+        print("Среднее число: {}".format(int(b) if b.is_integer() else b))
+    else:
+        print("Среднее число: {}".format(int(c) if c.is_integer() else c))
